2ea53abda43f662e13a9db8f3caa8ae39bc61076
HighLibrarian <HighLibrarian@moredroids.xyz>
2026-06-26 13:59:24 +0200

    add waypaper, polkitagent, discord

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -7,9 +7,14 @@
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function () 
+  --hl.exec_cmd("dbus-update-activation-environment --systemd --all")
+  hl.exec_cmd("awww-daemon") -- wallpaper service
+  hl.exec_cmd("waypaper --restore") -- wallpaper service
   hl.exec_cmd("waybar") -- our top bar
   hl.exec_cmd("swaync") -- notifications
-  hl.exec_cmd("awww-daemon") -- wallpaper service
   hl.exec_cmd("hypridle") -- idle manager that triggers hyprlock when needed
+  hl.exec_cmd("/usr/libexec/hyprpolkitagent")
+  hl.exec_cmd("flatpak run discord.discordapp.Discord")
+  
 end
 )
