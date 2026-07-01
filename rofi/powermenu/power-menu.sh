df6b07b54f282ba7677e6e112f04eb1f67376258
HighLibrarian <HighLibrarian@moredroids.xyz>
2026-07-01 18:22:42 +0200

    refactor power-menu.sh: streamline command execution and enhance session management

--- rofi/powermenu/power-menu.sh
+++ rofi/powermenu/power-menu.sh
@@ -18,74 +18,68 @@ lastlogin="`last $USER | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7`"
 uptime="`uptime -p | sed -e 's/up //g'`"
 host=`hostname`
 
+# Helpers
+is_hyprland="${XDG_CURRENT_DESKTOP:-}${DESKTOP_SESSION:-}"
+
+has_cmd() {
+	command -v "$1" >/dev/null 2>&1
+}
+
 # Options
-hibernate='󰤄'
+hibernate='󰤄' # for longer breaks
+suspend='󱖐'   # for short breaks.
 shutdown='󰐥'
 reboot='󰜉'
+sessionrestart=''   # restart the hyprland session
 lock='󱅞'
-suspend='󱖐'
 logout='󰍃'
-yes=''
-no='󰬟'
 
 # Rofi CMD
 rofi_cmd() {
 	rofi -dmenu \
+		-me-select-entry '' \
+		-me-accept-entry MousePrimary \
 		-p "  $USER@$host" \
 		-mesg "    Last Login: $lastlogin | 󰔛  Uptime: $uptime" \
 		-theme ${dir}/${theme}.rasi
 }
 
-# Confirmation CMD
-confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
-}
-
-# Ask for confirmation
-confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
-}
-
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$hibernate\n$reboot\n$shutdown" | rofi_cmd
+	echo -e "$shutdown\n$reboot\n$lock\n$logout\n$sessionrestart\n$suspend\n$hibernate" | rofi_cmd
+
 }
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
+	if [[ $1 == '--shutdown' ]]; then
+		if has_cmd hyprshutdown && [[ "$is_hyprland" =~ [Hh]yprland ]]; then
+			hyprshutdown -t 'Shutting down...' --post-cmd 'systemctl poweroff'
+		else
 			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--hibernate' ]]; then
-			systemctl hibernate
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
 		fi
-	else
-		exit 0
+	elif [[ $1 == '--reboot' ]]; then
+		if has_cmd hyprshutdown && [[ "$is_hyprland" =~ [Hh]yprland ]]; then
+			hyprshutdown -t 'Restarting...' --post-cmd 'systemctl reboot'
+		else
+			systemctl reboot
+		fi
+	elif [[ $1 == '--hibernate' ]]; then
+		systemctl hibernate
+	elif [[ $1 == '--suspend' ]]; then
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
+	elif [[ $1 == '--logout' ]]; then
+		if has_cmd hyprshutdown && [[ "$is_hyprland" =~ [Hh]yprland ]]; then
+			hyprshutdown
+		elif has_cmd loginctl; then
+			loginctl terminate-user "$USER"
+		fi
+	elif [[ $1 == '--sessionrestart' ]]; then
+		if has_cmd hyprctl && [[ "$is_hyprland" =~ [Hh]yprland ]]; then
+			hyprctl dispatch exit
+		fi
 	fi
 }
 
@@ -101,8 +95,13 @@ case ${chosen} in
     $hibernate)
 		run_cmd --hibernate
         ;;
+		$sessionrestart)
+		run_cmd --sessionrestart
+				;;
     $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+		if has_cmd hyprlock && [[ "$is_hyprland" =~ [Hh]yprland ]]; then
+			hyprlock
+		elif [[ -x '/usr/bin/betterlockscreen' ]]; then
 			betterlockscreen -l
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
