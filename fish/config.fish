4e164f17e070aca4f76263bf741b715e635af717
HighLibrarian <HighLibrarian@moredroids.xyz>
2026-07-20 22:45:54 +0200

    add aliases and update PATH for LM Studio CLI in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,14 @@ end
 set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
 starship init fish | source
 set -U fish_user_paths $fish_user_paths ~/.local/bin
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/bdw/.lmstudio/bin
+# End of LM Studio CLI section
+
+alias ls "eza -l --icons"
+
+alias gtrep "cd ~/Repositories"
+alias gtdl "cd ~/Downloads"
+alias gthome "cd ~"
+alias cls "clear"
